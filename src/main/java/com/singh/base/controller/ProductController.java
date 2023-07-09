4b64027aaa71d3263206b52ab22e64b6d1c74168
RohitSingh <thakurrohitdsingh@gmail.com>
2023-07-10 01:21:42 +0530

    Added @RequestMapping(value = "/product_ms")
    for API Gateway

--- src/main/java/com/singh/base/controller/ProductController.java
+++ src/main/java/com/singh/base/controller/ProductController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +40,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
+@RequestMapping(value = "/product_ms")
 public class ProductController {
 	
 	@Autowired
